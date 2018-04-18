1d3bee29a7fdd60c2ee354449d796c9deb882522
jamboljack <jama.muttaqin@gmail.com>
2018-04-18 22:43:33 +0700

    Social

--- application/controllers/admin/Social.php
+++ application/controllers/admin/Social.php
@@ -32,13 +32,13 @@ class Social extends CI_Controller
 
         foreach ($List as $r) {
             $no++;
-            $row     = array();
+            $row       = array();
             $social_id = $r->social_id;
 
-            $row[] = '  <button type="button" class="btn btn-primary btn-xs" title="Edit Data" href="javascript:void(0)" onclick="edit_data('."'".$social_id."'".')">
+            $row[] = '  <button type="button" class="btn btn-primary btn-xs" title="Edit Data" href="javascript:void(0)" onclick="edit_data(' . "'" . $social_id . "'" . ')">
                         <i class="fa fa-edit"></i>
                         </button>
-                        <a onclick="hapusData('.$social_id.')">
+                        <a onclick="hapusData(' . $social_id . ')">
                             <button class="btn btn-danger btn-xs" type="button" title="Delete Data">
                             <i class="fa fa-times-circle"></i>
                             </button>
@@ -48,7 +48,7 @@ class Social extends CI_Controller
             $row[] = $r->social_name;
             $row[] = $r->social_class;
             $row[] = $r->social_url;
-            
+
             $data[] = $row;
         }
 
@@ -67,8 +67,9 @@ class Social extends CI_Controller
         $this->social_m->insert_data();
     }
 
-    public function get_data($id) {
-        $data   = $this->social_m->select_by_id($id)->row();
+    public function get_data($id)
+    {
+        $data = $this->social_m->select_by_id($id)->row();
         echo json_encode($data);
     }
 
